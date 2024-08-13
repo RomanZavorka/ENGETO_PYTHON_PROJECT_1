2b4bfce45cbe3eff9fb85259e9233c4111b84a6b
RomanZavorka <romanz90zero@gmail.com>
2024-08-13 21:29:38 +0200

    Add

--- python_project_1.py
+++ python_project_1.py
@@ -44,23 +44,26 @@ garpike and stingray are also present.'''
 user_login = input("Please insert your username: \n")
 
 if user_login in registered_users:
-    print(60*"-")
-    if input(
+    if input(60*"-" + "\n"
             f"Please, insert your login password: \n"
         ) == registered_users[user_login]:
-        print(60*"-")
-        print(f"Welcome to the app, {user_login}.")
-        print("We have 3 texts to be analyzed.")
-        print(60*"-")
+        print(f'{60*"-"} \n'
+              f"Welcome to the app, {user_login}.\n"
+              f"We have 3 texts to be analyzed.\n"
+              f'{60*"-"}'
+        )
+        
         text_choice = str(input("Please enter a number btw. 1 and 3 to select: \n"))
         if text_choice.isnumeric() == True:
             text_choice = int(text_choice) - 1
         
         if text_choice in (0,1,2):
             TEXTS = TEXTS[text_choice]
-            print(60*"-")
-            print(f'You have chosen text number {text_choice + 1}: \n"{TEXTS}"')
-            print(60*"-")
+            print(f'{60*"-"} \n'
+                  f'You have chosen text number {text_choice + 1}:\n' 
+                  f'"{TEXTS}" \n'
+                  f'{60*"-"}'
+            )
 
             titlecase_words = []
             uppercase_words = []
@@ -83,56 +86,25 @@ if user_login in registered_users:
                     if word.isnumeric() == True:
                             numeric_strings.append(int(word))
             
-            len_1 = []
-            len_2 = []
-            len_3 = []
-            len_4 = []
-            len_5 = []
-            len_6 = []
-            len_7 = []
-            len_8 = []
-            len_9 = []
-            len_10 = []
-            len_11 = []
-            
+            words_length = [[],[],[],[],[],[],[],[],[],[],[]]
+
             for words in TEXTS.split(" "):
                 for word in words.split("\n"):
                     word = word.strip(".").strip(",")
-                    if len(word) == 1:
-                        len_1.append(word)
-                    elif len(word) == 2:
-                        len_2.append(word)
-                    elif len(word) == 3:
-                        len_3.append(word)
-                    elif len(word) == 4:
-                        len_4.append(word)
-                    elif len(word) == 5:
-                        len_5.append(word)
-                    elif len(word) == 6:
-                        len_6.append(word)
-                    elif len(word) == 7:
-                        len_7.append(word)
-                    elif len(word) == 8:
-                        len_8.append(word)
-                    elif len(word) == 9:
-                        len_9.append(word)
-                    elif len(word) == 10:
-                        len_10.append(word)
-                    elif len(word) >= 11:
-                        len_11.append(word)
-         
-            for word in uppercase_words:
-                if word in titlecase_words:
-                    titlecase_words.remove(word)
+                    if len(word) <= 11:
+                        words_length[len(word)-1].append(word)
+                    else:
+                        words_length[10].append(word)
 
-            print(f"There are {len(TEXTS.split())} words in the selected text.")
-            print(f"There are {len(titlecase_words)} titlecase words.")
-            print(f"There are {len(uppercase_words)} uppercase words.")
-            print(f"There are {len(lowercase_words)} lowercase words.")
-            print(f"There are {len(numeric_strings)} numeric strings.")
-            print(f"The sum of all the numeric strings is {sum(numeric_strings)}")
-            print(60*"-")
-            print(f"LEN|{5*' '}OCCURENCES{5*' '}|NR.")
+            print(f"There are {len(TEXTS.split())} words in the selected text.\n"
+                  f"There are {len(titlecase_words)} titlecase words.\n"
+                  f"There are {len(uppercase_words)} uppercase words.\n"
+                  f"There are {len(lowercase_words)} lowercase words.\n"
+                  f"There are {len(numeric_strings)} numeric strings.\n"
+                  f"The sum of all the numeric strings is {sum(numeric_strings)}\n"
+                  f"{60*'-'}\n"
+                  f"LEN|{5*' '}OCCURENCES{5*' '}|NR."
+            )
 
             words_sorted = 0
 
@@ -163,31 +135,24 @@ if user_login in registered_users:
                 def heading_break (offset, heading_text, break_length):
                     heading_text_length = len(heading_text)
                     print(f"{offset*'-'} {heading_text} "
-                        f"{(break_length-heading_text_length-offset-2)*'-'}"
+                          f"{(break_length-heading_text_length-offset-2)*'-'}"
                     )
 
                 print(60*"-")
                 heading_break(3, "List of words by string type", 60)
-                print(f"Titlecase words: {titlecase_words}")
-                print(f"Uppercase words: {uppercase_words}")
-                print(f"Lowercase words: {lowercase_words}")
-                print(f"Numeric strings: {numeric_strings}")
+                print(f"Titlecase words: {titlecase_words}\n"
+                      f"Uppercase words: {uppercase_words}\n"
+                      f"Lowercase words: {lowercase_words}\n"
+                      f"Numeric strings: {numeric_strings}\n"
+                      f"{60*'-'}"
+                )
                 
-                print(60*"-")
                 heading_break(3, "List of words by length", 60)
-                print(f"1 letter: {len_1}")
-                print(f"2 letters: {len_2}")
-                print(f"3 letters: {len_3}")
-                print(f"4 letters: {len_4}")
-                print(f"5 letters: {len_5}")
-                print(f"6 letters: {len_6}")
-                print(f"7 letters: {len_7}")
-                print(f"8 letters: {len_8}")
-                print(f"9 letters: {len_9}")
-                print(f"10 letters: {len_10}")
-                print(f"11+ letters: {len_11}")
-            
-        
+                
+                print(f"1 letter: {words_length[0]}")
+                for number in range (1,10):
+                    print(f"{number+1} letters: {words_length[number]}")
+                print(f"11+ letters: {words_length[10]}")
         else:
             print(60*"-")
             print("Incorrect choice, shutting down.")
